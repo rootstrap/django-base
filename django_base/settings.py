1365ea9c205e53ab21ee17b556c7f804408f48be
Kalil de Lima <kalil@rootstrap.com>
2019-06-04 16:45:30 -0300

    Add jwt auth to default authentication classes

--- django_base/settings.py
+++ django_base/settings.py
@@ -83,6 +83,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'django_base.wsgi.application'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     )
